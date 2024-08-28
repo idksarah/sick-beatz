e1f043d3611ea9e10824a321b88f014a4e13dbeb
idksarah <sarah.p.ngai@gmail.com>
2024-08-27 23:10:38 -0700

    Add input and start storeSong()

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("hello world!");
+
+const input = document.querySelector("#file-upload");
+
+function storeSong() {
+    let song = input.value;
+    alert(song);
+}
+
+input.addEventListener('click', () => {
+    if(input.value != undefined){
+        storeSong();
+    }
+})
