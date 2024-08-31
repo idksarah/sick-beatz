681b3ded89826e57d8a1359d3e5c49d409c3d68e
idksarah <sarah.p.ngai@gmail.com>
2024-08-30 23:49:49 -0700

    Adjust storeSong and eventListener; process filename and songlength

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-console.log("hello world!");
-
-const input = document.querySelector("#file-upload");
-
+var input = document.querySelector("#file-upload");
+var song;
 function storeSong() {
-    let song = input.value;
-    alert(song);
-}
-
-input.addEventListener('click', () => {
-    if(input.value != undefined){
-        storeSong();
+    if (input.files && input.files.length > 0) {
+        song = input.files[0]; //accesses the first file
     }
-})
+}
+input.addEventListener('change', function () {
+    storeSong();
+    if (song) {
+        handleAudioFile(song);
+    }
+});
+function handleAudioFile(file) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        var arrayBuffer = event.target.result;
+        var audioContext = new (window.AudioContext)();
+        audioContext.decodeAudioData(arrayBuffer, function (buffer) {
+            var duration = buffer.duration;
+            console.log('length' + duration);
+        });
+    };
+    reader.readAsArrayBuffer(file);
+    var url = URL.createObjectURL(file);
+    var audio = new Audio(url);
+    audio.onloadedmetadata = function () {
+        console.log("title:" + file.name);
+    };
+}
--- index.ts
+++ index.ts
@@ -1,14 +1,36 @@
-console.log("hello world!");
-
-const input = document.querySelector("#file-upload");
+const input = document.querySelector<HTMLInputElement>("#file-upload");
+let song;
 
 function storeSong() {
-    let song = input.value;
-    alert(song);
+    if(input.files && input.files.length > 0) {
+        song = input.files[0]; //accesses the first file
+    }
 }
 
-input.addEventListener('click', () => {
-    if(input.value != undefined){
-        storeSong();
+input.addEventListener('change', () => {
+    storeSong();
+    if(song){
+        handleAudioFile(song);
     }
 })
+
+function handleAudioFile(file){
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        const arrayBuffer = event.target.result as ArrayBuffer;
+        const audioContext = new (window.AudioContext)();
+        audioContext.decodeAudioData(arrayBuffer, function(buffer) {
+            const duration = buffer.duration;
+            console.log('length' + duration);
+        });
+    };
+
+    reader.readAsArrayBuffer(file);
+
+    const url = URL.createObjectURL(file);
+    const audio = new Audio(url);
+
+    audio.onloadedmetadata = function() {
+        console.log("title:" + file.name);
+    };
+}
